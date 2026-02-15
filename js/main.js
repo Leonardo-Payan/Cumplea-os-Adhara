c2ef051a91728b1d36d9a8ca1e108c38320f8b95
Leonardo-Payan <leonardopayan22@gmail.com>
2026-02-15 13:41:48 -0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-
 onload = () => {
+  const audio = new Audio('../las-mañanitas.mp3');
+  audio.volume = 0.5;
+  audio.play();
+
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
-
     const titles = ('Feliz cumpleaños!!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
-
     function appendTitle() {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 300); // 300ms delay
       }
     }
-
     appendTitle();
-
     clearTimeout(c);
   }, 1000);
 };
